2102eb67c40bced3c4e9df9d554b89639a93834e
FaroukBoussarsar <faroukboussarsar@gmail.com>
2020-10-19 12:15:38 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const About=() =>{
+    return (
+        <Layout>
+      <div>
+      
+          <h1> about next js</h1>
+          <p>hello and welcome my first app</p>
+      </div>
+      </Layout>
+    )
+  }
+  export default About
+  
